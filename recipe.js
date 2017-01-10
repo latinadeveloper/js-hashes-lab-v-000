06273e5137cab08b83a09e587637d9984723e445
Isis Tejeda <isis@isistejeda.com>
2017-01-10 11:36:47 -0600

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe, ingredient, value){
+  recipe[ingredient] = value;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, value){
+  recipe[ingredient] = value;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var key in recipe) {
+  console.log("this recipe calls for " + recipe[key] + " of " + key)
+};
+}
